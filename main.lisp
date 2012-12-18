1723700c8a3f5791f3548e38bb695bf8951ea84f
myrna1988 <myrnavandeburgwal@hotmail.com>
2012-12-18 20:54:58 +0100

    Update main.lisp

--- main.lisp
+++ main.lisp
@@ -13,22 +13,60 @@
 ;;;; generate population
 
 ;; the list 'pop' starts empty but adds individuals by calling genList
-(let ((pop nil))
- (defun genPop (p)
-  (dotimes (i p pop) (setq pop (cons (genList) pop))) ))
+(defun gen-pop (pop-size fitness-fun)
+ (let* ((individual (gen-ind 100)) 
+       (fitness (case fitness-fun 
+             (1 (co individual)) 
+             (2 (sco individual))
+             (3 (d-trapfun individual))
+             (4 (n-trapfun individual)) )))
+  (if (> pop-size 0)
+   (cons (list individual fitness) (gen-pop (- pop-size 1) fitness-fun))
+    nil)))
 
 ;; this function generates a list of 100 bits
-(let ((list (make-list 0)))
- (defun genList ()
-  (dotimes (i 100 list) (setq list (cons (random 2) list))) ))
+(defun gen-ind (ind-size)
+ (if (> ind-size 0)
+  (cons (random 2) (gen-ind (- ind-size 1)))
+   nil))
 
-  
+
 ;;;; fitness functions
   
-;; fitness function 1 (co)
-;; fitness function 2 (sco)
-;; fitness function 3 (d=1)
-;; fitness function 4 (d=2.5)
+(defun co (individual)
+ (if individual
+  (+ (first individual) (co (rest individual)))
+   0))
+
+(defun sco (individual) (sco2 individual 1))
+
+(defun sco2 (individual bit)
+ (if individual
+  (+ (* (first individual) bit) (sco2 (rest individual) (+ bit 1)))
+   0)) 
+
+(defun d-trapfun (individual) (trapfun individual 4 1))
+
+(defun n-trapfun (individual) (trapfun individual 4 2.5))
+
+(defun trapfun (individual k d)
+ (let ((sublist (sub individual nil k)))
+  (+ (b (first sublist) k d) (trapFun (rest sublist) k d)) ))
+
+
+;; sublist of length k that takes the first k bits of individual
+(defun sub (individual sublist k)
+ (if (> k 0)
+  (sub (rest individual) (append sublist (list (first individual))) (- k 1))
+   (list individual sublist) ))
+
+(defun b (sublist k d)
+ (let ((count (co sublist)))
+  (if (= count k)
+   k  
+    (- (- k d) (* (/ (- k d) (- k 1)) count)) )))
+
+
 
 ;; sublist that will be called often
 (defun sublist (n L)
